a0c72829f1b2b2db736cf48c09354f99417c1d88
tutorialesvirtuales <juanpac05@hotmail.com>
2019-07-02 11:52:35 -0500

    Curso laravel parte 12

---
+++ resources/views/admin/permiso/crear.blade.php
@@ -0,0 +1,20 @@
+@extends("theme.$theme.layout")
+
+@section('titulo')
+    Permisos
+@endsection
+
+@section('contenido')
+<div class="row">
+    <div class="col-lg-12">
+        <div class="box box-danger">
+            <div class="box-header with-border">
+                <h3 class="box-title">Crear Permiso</h3>
+            </div>
+            <div class="box-body">
+                Formulario Here
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/admin/permiso/index.blade.php
@@ -0,0 +1,39 @@
+@extends("theme.$theme.layout")
+
+@section('titulo')
+    Permisos
+@endsection
+
+@section('contenido')
+<div class="row">
+    <div class="col-lg-12">
+        <div class="box box-primary">
+            <div class="box-header with-border">
+                <h3 class="box-title">Permiso</h3>
+            </div>
+            <div class="box-body table-responsive no-padding">
+                <table class="table table-bordered table-hover table-striped">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nombre</th>
+                            <th>Slug</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($permisos as $permiso)
+                            <tr>
+                                <th>{{$permiso->id}}</th>
+                                <th>{{$permiso->nombre}}</th>
+                                <th>{{$permiso->slug}}</th>
+                                <th></th>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
