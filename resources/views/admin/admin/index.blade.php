7045505e5ded2fc63bf3799005ba9b895a1ed97b
tutorialesvirtuales <juanpac05@hotmail.com>
2019-07-07 00:49:35 -0500

    Autenticacion Clase 20

---
+++ resources/views/admin/admin/index.blade.php
@@ -0,0 +1,10 @@
+@extends("theme.$theme.layout")
+@section("titulo")
+Aministrador
+@endsection
+
+@section('contenido')
+<div class="row">
+    <div class="col-lg-2">Bienvenidos</div>
+</div>
+@endsection
